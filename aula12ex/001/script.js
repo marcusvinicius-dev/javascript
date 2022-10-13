369d5be5a875db89f55c89f899eef70e91c99379
Marcus Vinicius <112499546+marcusvinicius-dev@users.noreply.github.com>
2022-10-13 10:31:51 -0300

    atualizacoes

---
+++ aula12ex/001/script.js
@@ -0,0 +1,21 @@
+var agora = new Date()
+var hora = agora.getHours()
+var txthora = window.document.getElementById('Thora')
+var imag = window.document.getElementById('imagem')
+
+
+
+
+
+txthora.innerHTML = `Agora são <strong>${hora}</strong> horas.`
+
+if (hora < 12){
+    imag.setAttribute("src", "bom-dia.jpg")
+    document.body.style.background = "#CDBB91"
+} else if (hora < 18){
+    imag.setAttribute("src", "boa-tarde.jpg")
+    document.body.style.background = "#A97964"
+}else{
+    imag.setAttribute("src", "boa-noite.jpg")
+    document.body.style.background = "#4A4B4C"
+}
