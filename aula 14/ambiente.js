6b3025ddad9bece3cdb6e54b0e344a63aef9a488
Marcus Vinicius <112499546+marcusvinicius-dev@users.noreply.github.com>
2022-10-18 10:57:27 -0300

    Create ambiente.js

---
+++ aula 14/ambiente.js
@@ -0,0 +1,15 @@
+/* 
+var c = 1;
+while (c <= 10){
+    console.log(`contou ${c}`)
+    c++
+}
+*/
+
+//-----------------------
+console.log(`Vai começar!`)
+for (var c = 1 ; c <= 10 ; c++){
+    console.log(`contou ${c}`)
+}
+
+console.log(`FIM!`)
