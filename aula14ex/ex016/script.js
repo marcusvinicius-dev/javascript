08bbea138638476f5c9e008e529e54002823d03f
Marcus Vinicius <112499546+marcusvinicius-dev@users.noreply.github.com>
2022-10-21 17:39:07 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,30 +1,33 @@
 function contar(){
-    var ini = window.document.getElementById('txtInicio')
-    var fim = window.document.getElementById('txtFim')
-    var pas = window.document.getElementById('txtPasso')
+    var initxt = window.document.getElementById('txtInicio')
+    var ini = Number(initxt.value)
+    var fimtxt = window.document.getElementById('txtFim')
+    var fim = Number(fimtxt.value)
+    var pastxt = window.document.getElementById('txtPasso')
+    var pas = Number(pastxt.value)
     var res = window.document.getElementById('txtRes')
-    var c = 0
     
     
-    if(Number(ini.value <= 0)){
+    if(ini <= 0){
         window.alert(`O valor INICIO deve ser Maior que 0`)
     } 
     
-    if(Number(fim.value) <= Number(ini.value)){
+    if(fim <= ini){
         window.alert(`O valor FIM deve ser Maior que o valor INICIO`)
     }
 
-    if(pas.value < 1){
-        window.alert(`O Valor PASSO deve ser Maior que 0`)
+    if(pas < 1){
+        window.alert(`O Valor PASSO deve ser Maior que 0! Vamos considerar como 1`)
+        pas = 1
     }
 
+    res.innerHTML = `Contando: <br> &#x1F3C1; `
 
-    res.innerHTML = `Contando: `
+    while (ini <= fim){
+        res.innerHTML += `${ini} &#x1F449`
+        ini += pas
+    }
 
-    while (c <= fim.value){
-        res.innerHTML += `${ini.value}`
-        Number(ini) = ini.value + pas.value
-        c = c + pas.value
-   }
+        res.innerHTML += `&#x1F3C1`
 
     }
